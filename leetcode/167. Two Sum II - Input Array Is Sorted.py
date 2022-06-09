6452a564a5245227cba81f20287444f6c4f19dd4
Daniil <daniil@vivaldi.net>
2022-06-09 20:59:30 +0800

    Find sum in a sorted array

---
+++ leetcode/167. Two Sum II - Input Array Is Sorted.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, numbers, target):
+        left = 0
+        right = len(numbers) - 1
+
+        while True:
+            current_sum = numbers[left] + numbers[right]
+
+            if current_sum > target:
+                right -= 1
+            elif current_sum < target:
+                left += 1
+            else:
+                return [left + 1, right + 1]
